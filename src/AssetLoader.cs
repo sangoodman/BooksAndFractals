fc7fbcd43172be0416587259e46e821f45098f06
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-04-03 18:18:48 -0700

    Added more methods for Asset Bundles management.

--- src/AssetLoader.cs
+++ src/AssetLoader.cs
@@ -9,6 +9,8 @@ namespace BooksAndFractals
     /// </summary>
     public static class AssetLoader
     {
+        static Dictionary<string, Il2CppAssetBundle> loadedBundles = new();
+
         /// <summary>
         /// Loads an object inside of the asset bundle of the specified name.
         /// </summary>
@@ -16,7 +18,7 @@ namespace BooksAndFractals
         /// <param name="AssetName">The name of the OBJECT to load.</param>
         /// <param name="Folder">The folder where the asset bundle file is located. (Needs to be inside of the Mods folder).</param>
         /// <param name="FileName">The name of the Asset Bundle to load.</param>
-        /// <returns></returns>
+        /// <returns>The loaded object if it isn't null.</returns>
         public static T LoadAsset<T>(string AssetName, string Folder, string FileName) where T: Object
         {
             string bundlePath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, "Mods", Folder, FileName);
@@ -39,6 +41,75 @@ namespace BooksAndFractals
             returningAsset.hideFlags = HideFlags.DontUnloadUnusedAsset;
             bundle.Unload(false);
             return returningAsset != null ? returningAsset : null;
+        }
+
+        /// <summary>
+        /// Preloads a bundle in memory so you can load assets from it later.
+        /// </summary>
+        /// <param name="bundlePath">The path to the Asset Bundle file.</param>
+        public static void PreloadBundle(string bundlePath)
+        {
+            if (!File.Exists(bundlePath))
+            {
+                Debug.LogError($"The specified asset path \"{bundlePath}\" doesn't exist.");
+                return;
+            }
+
+            Il2CppAssetBundle bundle = Il2CppAssetBundleManager.LoadFromFile(bundlePath);
+
+            loadedBundles.Add(Path.GetFileNameWithoutExtension(bundlePath), bundle);
+        }
+        /// <summary>
+        /// Preloads a bundle in memory so you can load assets from it later.
+        /// </summary>
+        /// <param name="folder">The folder where the asset bundle file is located. (Needs to be inside of the Mods folder).</param>
+        /// <param name="fileName">he name of the Asset Bundle to load.</param>
+        public static void PreloadBundle(string folder, string fileName)
+        {
+            string bundlePath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, "Mods", folder, fileName);
+            PreloadBundle(bundlePath);
+        }
+
+        /// <summary>
+        /// Loads an object from one of the currently loaded asset bundles with the specified name.
+        /// </summary>
+        /// <typeparam name="T">The type of object to return.</typeparam>
+        /// <param name="assetName">The name of the asset to load.</param>
+        /// <param name="bundleName">The name of the loaded bundle (the name of the bundle file).</param>
+        /// <returns>The loaded object if it exists.</returns>
+        public static T LoadAsset<T>(string assetName, string bundleName) where T : Object
+        {
+            if (!loadedBundles.ContainsKey(bundleName))
+            {
+                Debug.Log("Couldn't find loaded asset bundle with name:" + bundleName);
+                return null;
+            }
+
+            T assetToReturn = loadedBundles[bundleName].Load<T>(assetName);
+            if (assetToReturn == null)
+            {
+                Debug.LogError("Error loading the asset of name: " + assetName);
+                return null;
+            }
+
+            return assetToReturn;
+        }
+
+        /// <summary>
+        /// Unloads the specified asset bundle from memory.
+        /// </summary>
+        /// <param name="bundleName">The name of the bundle to unload.</param>
+        /// <param name="unloadAssets">Should the already loaded assets from this bundle be unloaded as well?</param>
+        public static void UnloadBundle(string bundleName, bool unloadAssets = false)
+        {
+            if (!loadedBundles.ContainsKey(bundleName))
+            {
+                Debug.Log("Couldn't find loaded asset bundle with name:" + bundleName);
+                return;
+            }
+
+            loadedBundles[bundleName].Unload(unloadAssets);
+            loadedBundles.Remove(bundleName);
         }
     }
 }
